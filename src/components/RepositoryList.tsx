3597a2b88cf3844099da8f027059e4db2bbba006
Jairo Tunisse <jairo_tunisse@outlook.com>
2021-11-25 20:33:19 -0300

    improviments: module done :heavy_check_mark:

--- src/components/RepositoryList.tsx
+++ src/components/RepositoryList.tsx
@@ -5,8 +5,14 @@ import '../styles/repositories.scss';
 
 //https://api.github.com/orgs/rocketseat/repos
 
+interface Repository {
+    name: string;
+    description: string; 
+    html_url: string;
+}
+
 export function RespositoryList() {
-    const [repositories, setRepositories] = useState([]);
+    const [repositories, setRepositories] = useState<Repository[]>([]);
 
     useEffect(() => {
         fetch('https://api.github.com/orgs/rocketseat/repos')
